9ebfdeef200bbda6314c08d3b3dc73feddebba1a
Eric McKinney <mckinney99@gmail.com>
2017-10-25 14:45:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  i = 0
+  array = []
+  while i < array.length
+  my_collect(array) do |name|
+    name.split(" ").first
+    array << yield(array[name])
+    i += 1
+  end
+array
+end
+end
